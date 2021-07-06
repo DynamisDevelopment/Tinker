08802794e82e5023ffc2be6201bdbbfcf48997ab
Joshua Hall <49533903+DynamisDevelopment@users.noreply.github.com>
2021-07-05 23:19:45 -0400

    Moved shared schemas into npm package

--- Backend/src/routers/users.js
+++ Backend/src/routers/users.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const User = require("../models/user")
-const auth = require("../middleware/auth")
-const { sendWelcomeEmail } = require("../emails/account")
+const express = require('express')
+const User = require('../schemas/user')
+const auth = require('../middleware/auth')
+const { sendWelcomeEmail } = require('../emails/account')
 
 const router = new express.Router()
 
-router.post("/users", async (req, res) => {
+router.post('/users', async (req, res) => {
   const user = new User(req.body)
   const token = await user.generateAuthToken()
 
@@ -19,7 +19,7 @@ router.post("/users", async (req, res) => {
   }
 })
 
-router.post("/users/login", async (req, res) => {
+router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
     const token = await user.generateAuthToken()
@@ -31,19 +31,15 @@ router.post("/users/login", async (req, res) => {
   }
 })
 
-router.post("/users/logout", auth, async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
   try {
-    req.user.tokens = req.user.tokens.filter(
-      (token) => token.token !== req.token
-    )
+    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
     await req.user.save()
     res.send()
   } catch (err) {
     res.status(500).send(err)
   }
 })
-
-
 
 // router.post("/users/logoutAll", auth, async (req, res) => {
 //   try {
@@ -55,21 +51,21 @@ router.post("/users/logout", auth, async (req, res) => {
 //   }
 // })
 
-router.get("/users/me", auth, async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.patch("/users/me", auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
-  const allowedUpdates = ["name", "email", "password", "age"]
-  const isValidOperation = updates.every((update) =>
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every(update =>
     allowedUpdates.includes(update)
   )
 
   try {
     const user = req.user
 
-    updates.forEach((update) => (user[update] = req.body[update]))
+    updates.forEach(update => (user[update] = req.body[update]))
 
     await user.save()
     res.send(user)
@@ -78,7 +74,7 @@ router.patch("/users/me", auth, async (req, res) => {
   }
 })
 
-router.delete("/users/me", auth, async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove()
     res.send(user)
